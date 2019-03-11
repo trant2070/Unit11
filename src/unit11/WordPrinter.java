327cbd0cad5a22a1f337ebd7a84b91ca52d386f6
Tyler Tran <trant2070@my.sduhsd.net>
2019-03-11 09:17:32 -0700

    Merge origin/master
    Conflicts:
    	README.md

---
+++ src/unit11/WordPrinter.java
@@ -0,0 +1,16 @@
+package unit11;
+
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+
+import static java.lang.System.*;
+
+public class WordPrinter
+{
+	//instance variables and constructors could be present, but are not necessary
+		
+	public static void printWord(String word, int times)
+	{
+	}
+}
